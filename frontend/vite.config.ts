5f8b6cbe906b86465a7164eefa552d93991dbd82
srj0407 <srj0407@gmail.com>
2025-02-12 16:50:36 -0800

    Trying to get the website working.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  logLevel: 'info',
   plugins: [react()],
   server: {
-    open: true, // This will open your browser automatically when you run npm run dev
+    open: true,
   },
-})
+});
