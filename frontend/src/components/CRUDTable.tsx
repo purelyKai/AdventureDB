04ce0ae820b61b9e728f8f110e74d55c7a76bf25
Kai Black <kaibblack03+github@gmail.com>
2025-02-15 17:17:32 -0800

    uses deployed backend url

--- frontend/src/components/CRUDTable.tsx
+++ frontend/src/components/CRUDTable.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const API_BASE_URL = "http://localhost:3001/api/";
+const API_BASE_URL = "https://adventuredb-backend.onrender.com/api/";
 
 interface Field {
   name: string;
