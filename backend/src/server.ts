c662e3ecbd867ffc13bb2134f85f51735a7c3db1
blackkai <blackkai@oregonstate.edu>
2025-02-26 21:45:50 -0800

    Overhaul to run on the school servers

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,7 @@ import { Data } from "./types/types";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.VITE_PORT || 3524;
 
 app.use(cors());
 app.use(express.json());
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5173", // Update this to your backend's port
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+  base: './',
+  build: {
+    outDir: '../../../public_html/AdventureDB',
   },
 });
