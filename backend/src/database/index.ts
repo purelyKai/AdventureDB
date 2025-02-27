ad064cde5651213f050b5185371aaae475201586
Kai Black <kaibblack03+github@gmail.com>
2025-02-26 20:08:58 -0800

    Don't explicitly state port in connection

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -10,7 +10,6 @@ async function createConnection() {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      port: 3306, // Explicitly set port
       ssl: { rejectUnauthorized: false }, // Disable strict SSL
     });
 
